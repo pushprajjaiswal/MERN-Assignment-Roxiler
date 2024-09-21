f07d5a93b1b16acbddb078a98177504f2efd9379
pushprajjaiswal <pushprajapp12@gmail.com>
2024-09-21 11:32:59 +0530

    Feat: Added express server setup

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/server.config');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+app.get('/ping', (req, res) => {
+    return res.json({message: 'Server is alive'});
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
